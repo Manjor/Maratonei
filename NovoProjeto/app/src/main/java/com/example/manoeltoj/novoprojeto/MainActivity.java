9159cfa28a38f834951f048c9b2f91361120ad8e
Manoel T. de Oliveira Junior <manoeltoj@tce-to.tce.to.gov.br>
2018-10-16 17:23:59 -0300

    Inclusão do Projeto de Animação com Lottie

---
+++ NovoProjeto/app/src/main/java/com/example/manoeltoj/novoprojeto/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.manoeltoj.novoprojeto;
+
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.utils.LottieValueAnimator;
+
+public class MainActivity extends AppCompatActivity{
+
+    LottieAnimationView play ;
+    LottieAnimationView loading;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        play = findViewById(R.id.lottiePlay);
+        loading = findViewById(R.id.lottieLoading);
+        button = findViewById(R.id.button);
+
+        //loading.playAnimation();
+    }
+    public void play(){
+        loading.playAnimation();
+    }
+}
