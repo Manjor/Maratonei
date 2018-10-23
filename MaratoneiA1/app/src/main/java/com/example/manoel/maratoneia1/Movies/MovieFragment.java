7773e15157febeed701d7dab9c4d2a8588291b64
Manjor <manoeltavares.jr@gmail.com>
2018-10-23 20:03:27 -0300

    Inclusão de Botões de Categorias

--- MaratoneiA1/app/src/main/java/com/example/manoel/maratoneia1/Movies/MovieFragment.java
+++ MaratoneiA1/app/src/main/java/com/example/manoel/maratoneia1/Movies/MovieFragment.java
@@ -1,7 +1,5 @@
 package com.example.manoel.maratoneia1.Movies;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -19,8 +17,6 @@ import android.widget.Toast;
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.manoel.maratoneia1.Configuracao;
 import com.example.manoel.maratoneia1.R;
-import com.example.manoel.maratoneia1.Series.Serie;
-import com.example.manoel.maratoneia1.Series.SerieAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,7 +30,6 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.EventListener;
 import java.util.List;
 
 public class MovieFragment extends Fragment implements View.OnClickListener {
@@ -47,8 +42,11 @@ public class MovieFragment extends Fragment implements View.OnClickListener {
     private LottieAnimationView lottieLoad;
     private Button btnNowPlay;
     private Button btnAction;
-    private Button btnTriller;
+    private Button btnAventure;
+    private Button btnComedy;
+    private Button btnRomance;
     private Button btnWar;
+    private Button btnMistery;
 
     @Nullable
     @Override
@@ -58,17 +56,29 @@ public class MovieFragment extends Fragment implements View.OnClickListener {
 
         recyclerView = view.findViewById(R.id.recycleMovie);
         lottieLoad = view.findViewById(R.id.lottieLoadMovie);
+
         btnNowPlay = view.findViewById(R.id.btnNowPlay);
         btnNowPlay.setOnClickListener(this);
 
         btnAction = view.findViewById(R.id.btnAction);
         btnAction.setOnClickListener(this);
 
-        btnTriller = view.findViewById(R.id.btnTriller);
-        btnTriller.setOnClickListener(this);
+        btnAventure = view.findViewById(R.id.btnAventure);
+        btnAventure.setOnClickListener(this);
+
+        btnComedy = view.findViewById(R.id.btnComedy);
+        btnComedy.setOnClickListener(this);
+
+        btnRomance = view.findViewById(R.id.btnRomance);
+        btnRomance.setOnClickListener(this);
+
+        btnMistery = view.findViewById(R.id.btnMistery);
+        btnMistery.setOnClickListener(this);
 
         btnWar = view.findViewById(R.id.btnWar);
         btnWar.setOnClickListener(this);
+
+
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
 
@@ -100,9 +110,23 @@ public class MovieFragment extends Fragment implements View.OnClickListener {
             case R.id.btnAction:
                 chamaTask(Configuracao.getMovieByGenre(28,getResources().getString(R.string.language)));
                 break;
-            case R.id.btnTriller:
+            case R.id.btnAventure:
                 chamaTask(Configuracao.getMovieByGenre(12,getResources().getString(R.string.language)));
                 break;
+            case R.id.btnComedy:
+                chamaTask(Configuracao.getMovieByGenre(35,getResources().getString(R.string.language)));
+                break;
+            case R.id.btnRomance:
+                chamaTask(Configuracao.getMovieByGenre(10749,getResources().getString(R.string.language)));
+                break;
+            case R.id.btnMistery:
+                chamaTask(Configuracao.getMovieByGenre(9648,getResources().getString(R.string.language)));
+                break;
+            case R.id.btnWar:
+                chamaTask(Configuracao.getMovieByGenre(10752,getResources().getString(R.string.language)));
+                break;
+
+
         }
     }
 
