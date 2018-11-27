72d1513dcbb98f16d58c327d9cedc78ca5a2462e
Manjor <manoeltavares.jr@gmail.com>
2018-11-27 21:22:47 -0200

    Inclusao de Thread para processamento de Requisições HTTP

--- MaratoneiKotlin/app/src/main/java/com/example/manoeltoj/maratoneikotlin/MainActivity.kt
+++ MaratoneiKotlin/app/src/main/java/com/example/manoeltoj/maratoneikotlin/MainActivity.kt
@@ -10,6 +10,9 @@ import android.support.v4.widget.NestedScrollView
 import android.support.v7.widget.Toolbar
 import android.telecom.Connection
 import android.widget.TableLayout
+import com.example.manoeltoj.maratoneikotlin.Movie.MovieFragment
+import com.example.manoeltoj.maratoneikotlin.R.string.movies
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,27 +30,31 @@ class MainActivity : AppCompatActivity() {
         toolbar = findViewById(R.id.toolbar)
         tabLayout = findViewById(R.id.tabs)
         viewPager = findViewById(R.id.pagerViewer)
-        setSupportActionBar(toolbar)
+        //setSupportActionBar(toolbar)
 
         toolbar.setTitle("Maratonei")
         toolbar.setTitleTextColor(resources.getColor(R.color.colorPrimaryDarkRoxo))
 
-        var viewPagerAdapter:ViewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+        var viewPagerAdapter: ViewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+        viewPagerAdapter.addFragment(MovieFragment(), resources.getString(movies))
+
 //        viewPagerAdapter.addFragment()
 //        viewPagerAdapter.addFragment()
-//        viewPagerAdapter.addFragment()
-
-        viewPager.setAdapter(viewPagerAdapter)
-        tabLayout.setupWithViewPager(viewPager)
-
-
+        try {
+            viewPager.setAdapter(viewPagerAdapter)
+            tabLayout.setupWithViewPager(viewPager)
+        } catch (e: Exception) {
+            println(e.printStackTrace())
+        }
     }
 
     companion object {
-        var CONNECTION:Boolean = false
+        var CONNECTION: Boolean = false
     }
+
     fun verifyConnection(): Boolean {
-        var connected: Boolean;
+
+        var connected: Boolean
         var conectivityManager: ConnectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         if (
                 conectivityManager.getActiveNetworkInfo() != null && conectivityManager.activeNetworkInfo.isConnected()
--- MaratoneiKotlin/app/src/main/java/com/example/manoeltoj/maratoneikotlin/Movie/MovieFragment.kt
+++ MaratoneiKotlin/app/src/main/java/com/example/manoeltoj/maratoneikotlin/Movie/MovieFragment.kt
@@ -2,6 +2,7 @@ package com.example.manoeltoj.maratoneikotlin.Movie
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -9,20 +10,46 @@ import android.view.ViewGroup
 import com.airbnb.lottie.LottieAnimationView
 
 import com.example.manoeltoj.maratoneikotlin.Config
+import com.example.manoeltoj.maratoneikotlin.MainActivity
+import com.example.manoeltoj.maratoneikotlin.Movie.dataMovie.MovieTask
+import com.example.manoeltoj.maratoneikotlin.Movie.dataMovie.ResultMovie
+import com.example.manoeltoj.maratoneikotlin.R.layout.fragment_movies
+import com.example.manoeltoj.maratoneikotlin.R.string.language
+import kotlinx.android.synthetic.main.fragment_movies.*
 
-class MovieFragment:Fragment(),View.OnClickListener{
+class MovieFragment : Fragment() {
 
 
-
-    lateinit var recyclerView:RecyclerView
-    lateinit var lottieLoader:LottieAnimationView
+    lateinit var recyclerView: RecyclerView
+    lateinit var viewr: View
+    lateinit var lottieLoader: LottieAnimationView
+    lateinit var movieList: ArrayList<ResultMovie>
+    var urlNowPlaying: String = "";
+    lateinit var movieTask: MovieTask
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
 
-    //var resultMovies:ArrayList<>
-    override fun onClick(v: View?) {
+        viewr = inflater.inflate(fragment_movies, container, false)
+        recyclerView = reclyclerMovie
+        urlNowPlaying = Config.getMovieNowPlayng(resources.getString(language))
+
+        var layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(context)
+        (layoutManager as LinearLayoutManager).orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = layoutManager
+
+        if (MainActivity.CONNECTION == false) {
+
+        } else {
+            movieTask = MovieTask(this)
+            movieTask.execute(urlNowPlaying)
+        }
+
+        return viewr
+    }
+    fun setData(movieList: ArrayList<ResultMovie>) {
+        this.movieList = movieList
+        var movieAdapter:MovieAdapter = MovieAdapter(this.movieList)
+        recyclerView.adapter = movieAdapter
 
     }
 
